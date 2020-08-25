1e7a50456cf2fda7aa0cb0c38dcf4b962ec4eaf3
felipelima555 <felipe.lds@live.com>
2020-08-24 21:46:13 -0300

    fix: Resolve warning about font import

--- client-web/src/styles.ts
+++ client-web/src/styles.ts
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap');
-
   * {
     margin: 0;
     padding: 0;
